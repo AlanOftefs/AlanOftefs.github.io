a44b05525d552c4fb913f8f3b21699f5b1f1564d
AlanOftefs <84021751+AlanOftefs@users.noreply.github.com>
2024-01-15 16:01:25 +0800

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,22 @@
-alert("1");
-let target = document.getElementById("HomePage");
-target.classList.add("active");
-alert("Yes");
+let home = document.getElementById("Home");
+let areas = document.getElementById("Areas");
+let culture = document.getElementById("Culture");
+let news = document.getElementById("News");
+let joinUs = document.getElementById("JoinUs");
+let all = [home, areas, culture, news, joinUs];
+
+const currentUrl = window.location.href;
+
+all.forEach(item => {
+    if (!!item) {
+        if (item.href === currentUrl) {
+            item.classList.remove("UnselectedNavItem");
+            item.classList.add('SelectedNavItem');
+        }
+        else {
+            item.classList.remove("SelectedNavItem");
+            item.classList.add('UnselectedNavItem');
+        }
+    }
+});
+
